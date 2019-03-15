8d6ffe90bed4681e41068d9d3c2a8af448e75150
shadiaali <shadiamalfoy@gmail.com>
2019-03-15 08:51:55 -0400

    new build

---
+++ js/components/LoginComponent.js
@@ -0,0 +1,75 @@
+export default {
+    template: `
+        <div class="container">
+            <div class="jumbotron roku-jumbotron">
+<h1 style="text-align:center;"><font color="#ff6600">L</font><font color="#bd0048">o</font><font color="#e0f30e">g</font><font color="#04a47a">i</font><font color="#24c2cb">n</font></h1>
+                <form id="loginForm">
+                    <div class="form-row align-items-center">
+                        <div class="offset-md-4 col-md-4 my-2">
+                            <p class="form-titles">Username:</p>
+                            <label class="sr-only" for="inlineFormInputName">Name</label>
+                            <input v-model="input.username" type="text" class="form-control" id="formUsername" required>
+                        </div>
+
+                        <div class="offset-md-4 col-md-4 my-2">
+                            <p class="form-titles">Password:</p>
+                            <label class="sr-only" for="inlineFormPassword">Name</label>
+                            <input v-model="input.password" type="password" class="form-control" id="formPassword" required>
+                        </div>
+
+                        <div class="offset-md-7 my-2">
+                            <button v-on:click.prevent="login()" type="submit" class="btn btn-primary" id="signInBtn">Sign In</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+     `,
+
+     data() {
+         return {
+             input: {
+                 username: "",
+                 password: ""
+             },
+
+         }
+     },
+
+     methods: {
+         login() {
+            //console.log(this.$parent.mockAccount.username);
+
+            if(this.input.username != "" && this.input.password != "") {
+            // fetch the user from the database and generate form data
+            let formData = new FormData();
+
+             formData.append("username", this.input.username);
+             formData.append("password", this.input.password);
+
+             let url = `./admin/scripts/admin_login.php`;
+
+             fetch(url, {
+                    method: 'POST',
+                    body: formData
+                })
+                 .then(res => res.json())
+                 .then(data => {
+                    if (typeof data != "object") { // means that we're not getting a user object back
+                        console.warn(data);
+                        console.error("authentication failed!");
+                        this.$emit("autherror", data);
+                    } else {
+                        this.$emit("authenticated", true, data[0]);
+                        this.$router.replace({ name: "users" });
+                    }
+                })
+             .catch(function(error) {
+                 console.log(error);
+             });
+        } else {
+                 console.log("A username and password needs to be entered");
+            }
+        }
+    }
+ }
--- js/main.js
+++ js/main.js
@@ -1,49 +1,100 @@
-(()=> {
-    const vm = new Vue({
-        el: '#app',
 
-        data: {
-          mainmessage : "Welcome to Roku Flashback!",
-            videodata : [],
-            singlemoviedata : [],
+import UsersComponent from './components/UsersComponent.js';
+import LoginComponent from './components/LoginComponent.js';
+import AdminComponent from './components/AdminComponent.js';
+import UserHomeComponent from './components/UserHomeComponent.js';
 
-            videotitle : "",
-            videosource : "",
-            videodescription : "",
-            showDetails : false
-        },
+let router = new VueRouter({
 
-        created : function() {
-        //get all of the movie data on a page load
-           this.fetchMovieData(null);
-       },
+  routes: [
+      { path: '/', redirect: { name: "login"} },
+      { path: '/login', name: "login", component: LoginComponent },
+      { path: '/users', name: 'users', component: UsersComponent },
+      { path: '/userhome', name: "home", component: UserHomeComponent, props: true },
+      { path: '/admin', name: 'admin', component: AdminComponent }
+  ]
+});
 
-       methods: {
+const vm = new Vue({
 
-      fetchMovieData(movie) {
-        url = movie ? `./includes/index.php?movie=${movie}` : './includes/index.php';
-//this is a ternary statement (shorthand for if/else). left of the : is true, right is false
+  data: {
+    socItems: [
 
-        fetch(url) // pass in the one or many queries
-          .then(res => res.json())
-          .then(data => {
-            if (movie) {
-              // getting one movie, so use the single array, store data in single result above
-              console.log(data);
-              this.singlemoviedata = data;
-              //initial data grab store in the videodata array
-            } else {
-              // push all the video (or portfolio content) into the video array
-              console.log(data);
-              this.videodata = data;
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+      {link:"http://www.twitter.com", id: "twitter", class: "fab fa-twitter"},
+      {link:"http://www.facebook.com", id: "facebook", class: "fab fa-facebook-square"},
+      {link:"http://www.instagram.com", id: "instagram", class: "fab fa-instagram"},
+      {link:"https://www.youtube.com/", id: "instagram", class: "fab fa-youtube"},
+
+    ],
+    authenticated: false,
+    administrator: false,
+
+    genericMessage: "hello from the parent",
+
+    mockAccount: {
+      username: "user",
+      password: "password"
+    },
+
+    user: [],
+
+    //currentUser: {},
+
+    toastmessage: "Login failed!"
+  },
+
+  created: function() {
+    // do a session check and set authenticated to true if the session still exists
+    // if the cached user exists, then just navigate to their user home page
+
+    // the localstorage session will persist until logout
+
+    if (localStorage.getItem("cachedUser")) {
+      let user = JSON.parse(localStorage.getItem("cachedUser"));
+      this.authenticated = true;
+      // params not setting properly, so this route needs to be debugged a bit...
+      this.$router.push({ name: "home", params: { currentuser: user }});
+    } else {
+      this.$router.push({ path: "/login"} );
+    }
+  },
+
+  methods: {
+    setAuthenticated(status, data) {
+      this.authenticated = status;
+      this.user = data;
+    },
+
+    popError(errorMsg) {
+      // set the error message string and show the toast notification
+      this.toastmessage = errorMsg;
+      $('.toast').toast('show');
+    },
+
+    logout() {
+      // delete local session
+      if (localStorage.getItem("cachedUser")) {
+        localStorage.removeItem("cachedUser");
       }
+      // push user back to login page
+      this.$router.push({ path: "/login" });
+      this.authenticated = false;
+
 
     }
-  });
+  },
 
-})();
+  router: router
+}).$mount("#app");
+
+router.beforeEach((to, from, next) => {
+  console.log('router guard fired!', to, from, vm.authenticated);
+
+  if (vm.authenticated == false) {
+    next("/login");
+  } else {
+    next();
+  }
+
+
+});
